93455ca18bb8eb43543db753fa4a13a96177774c
George Dilthey <george.dilthey@gmail.com>
2021-08-03 22:37:26 -0400

    commit tree 81d955864b3c0a91c06d49a45a06db746ed585ba

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /* Given Code, don't edit */
 
+
 document.querySelector("button").addEventListener("click", handleClick);
 
 function handleClick(e) {
@@ -10,4 +11,20 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(time) {
+  let hour = parseInt(time.split(":")[0])
+
+  if(hour < 12)
+    return "Good Morning";
+  else if(hour > 17)
+    return "Good Evening";
+  else
+    return "Good Afternoon";  
+}
 /* Write your implementation of displayMessage() */
+
+function displayMessage(message){
+  let element = document.getElementById("greeting")
+  element.textContent = message
+}
